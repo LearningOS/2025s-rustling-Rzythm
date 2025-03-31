a8dc30bf99f2eb5696039d01f03ad1267eee80c0
Rzythm <rzy213213222@gmail.com>
2025-03-31 13:10:46 +0800

    finished

--- exercises/algorithm/algorithm10.rs
+++ exercises/algorithm/algorithm10.rs
@@ -2,7 +2,6 @@
 	graph
 	This problem requires you to implement a basic graph functio
 */
-// I AM NOT DONE
 
 use std::collections::{HashMap, HashSet};
 use std::fmt;
@@ -29,19 +28,76 @@ impl Graph for UndirectedGraph {
         &self.adjacency_table
     }
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
-        //TODO
+        let (from, to, weight) = edge;
+    
+        self.add_node(from);
+        self.add_node(to);
+    
+        if let Some(adj_list) = self.adjacency_table_mutable().get_mut(from) {
+            let mut edge_exists = false;
+            for (adj_node, adj_weight) in adj_list.iter_mut() {
+                if adj_node == to {
+                    *adj_weight = weight;
+                    edge_exists = true;
+                    break;
+                }
+            }
+        
+            if !edge_exists {
+                adj_list.push((String::from(to), weight));
+            }
+        }
+    
+        if let Some(adj_list) = self.adjacency_table_mutable().get_mut(to) {
+            let mut edge_exists = false;
+            for (adj_node, adj_weight) in adj_list.iter_mut() {
+                if adj_node == from {
+                    *adj_weight = weight;
+                    edge_exists = true;
+                    break;
+                }
+            }
+        
+            if !edge_exists {
+                adj_list.push((String::from(from), weight));
+            }
+        }
     }
 }
+
 pub trait Graph {
     fn new() -> Self;
     fn adjacency_table_mutable(&mut self) -> &mut HashMap<String, Vec<(String, i32)>>;
     fn adjacency_table(&self) -> &HashMap<String, Vec<(String, i32)>>;
     fn add_node(&mut self, node: &str) -> bool {
-        //TODO
-		true
+        if self.contains(node) {
+            return false;
+        }
+
+        self.adjacency_table_mutable().insert(String::from(node), Vec::new());
+        true
+
     }
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
-        //TODO
+        let (from, to, weight) = edge;
+    
+        self.add_node(from);
+        self.add_node(to);
+    
+        if let Some(adj_list) = self.adjacency_table_mutable().get_mut(from) {
+            let mut edge_exists = false;
+            for (adj_node, adj_weight) in adj_list.iter_mut() {
+                if adj_node == to {
+                    *adj_weight = weight;
+                    edge_exists = true;
+                    break;
+                }
+            }
+        
+            if !edge_exists {
+                adj_list.push((String::from(to), weight));
+            }
+        }
     }
     fn contains(&self, node: &str) -> bool {
         self.adjacency_table().get(node).is_some()
--- exercises/algorithm/algorithm9.rs
+++ exercises/algorithm/algorithm9.rs
@@ -2,7 +2,6 @@
 	heap
 	This question requires you to implement a binary heap function
 */
-// I AM NOT DONE
 
 use std::cmp::Ord;
 use std::default::Default;
@@ -37,7 +36,20 @@ where
     }
 
     pub fn add(&mut self, value: T) {
-        //TODO
+        self.items.push(value);
+        self.count += 1;
+    
+        let mut idx = self.count;
+        while idx > 1 {
+            let parent = self.parent_idx(idx);
+        
+            if (self.comparator)(&self.items[parent], &self.items[idx]) {
+                break;
+            }
+        
+            self.items.swap(idx, parent);
+            idx = parent;
+        }  
     }
 
     fn parent_idx(&self, idx: usize) -> usize {
@@ -57,8 +69,22 @@ where
     }
 
     fn smallest_child_idx(&self, idx: usize) -> usize {
-        //TODO
-		0
+        if !self.children_present(idx) {
+            return 0;
+        }
+    
+        let left = self.left_child_idx(idx);
+        let right = self.right_child_idx(idx);
+    
+        if right > self.count {
+            return left;
+        }
+    
+        if (self.comparator)(&self.items[left], &self.items[right]) {
+            left
+        } else {
+            right
+        }    
     }
 }
 
@@ -79,13 +105,38 @@ where
 
 impl<T> Iterator for Heap<T>
 where
-    T: Default,
+    T: Default + Clone,
 {
     type Item = T;
 
     fn next(&mut self) -> Option<T> {
-        //TODO
-		None
+       if self.is_empty() {
+            return None;
+        }
+        
+        let result = self.items.swap_remove(1);
+        
+        if self.count == 1 {
+            self.count = 0;
+            return Some(result);
+        }
+        
+        self.count -= 1;
+        
+        let mut idx = 1;
+        
+        while self.children_present(idx) {
+            let child = self.smallest_child_idx(idx);
+            
+            if (self.comparator)(&self.items[idx], &self.items[child]) {
+                break;
+            }
+            
+            self.items.swap(idx, child);
+            idx = child;
+        }
+        
+        Some(result) 
     }
 }
 
